fcb0faa5cc8b4450bf13d4b9d929208482f5e1fc
Macailh <salvadorenriquegb@gmail.com>
2023-03-25 12:29:15 -0600

    Add delete method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 //import http from "http";
 
 let notes = [
@@ -22,11 +23,40 @@ let notes = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { "Content-Type": "application/json" });
+//   response.end(JSON.stringify(notes));
+// });
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello world</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  response.status(204).end();
 });
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+// app.listen(PORT);
+// console.log(`Server running on port ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server running in port ${PORT}`);
+});
